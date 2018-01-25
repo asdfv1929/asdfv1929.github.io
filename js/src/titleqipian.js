511f7aa2b773ea7f8713b09beef36d68e8e7fdd9
asdfv1929 <asdfv1929@163.com>
2018-01-26 00:02:06 +0800

    Site updated: 2018-01-26 00:01:44

---
+++ js/src/titleqipian.js
@@ -0,0 +1,17 @@
+<!--崩溃欺骗-->
+var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
