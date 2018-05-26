d8b5dbbb59d7bfe8ad7ad66983534296f78d5732
asdfv1929 <asdfv1929@163.com>
2018-05-26 14:16:42 +0800

    Site updated: 2018-05-26 14:16:38

--- aplayer/music.js
+++ aplayer/music.js
@@ -4,6 +4,12 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
+        name: "PDD洪荒之力",
+        artist: '徐梦圆',
+        url: 'http://up.mcyt.net/?down/39868.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
         name: '9420',
         artist: '麦小兜',
         url: 'http://up.mcyt.net/?down/45967.mp3',
