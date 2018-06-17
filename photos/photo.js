d951de5e52bf3cf92ce132404211415df07436f7
asdfv1929 <asdfv1929@163.com>
2018-06-17 11:55:47 +0800

    Site updated: 2018-06-17 11:55:41

--- photos/photo.js
+++ photos/photo.js
@@ -14,26 +14,33 @@ photo ={
         var end = page * this.offset;
         if (begin >= data.length) return;
         var html, imgNameWithPattern, imgName, imageSize, imageX, imageY, li = "";
-        for (var i = begin; i < end && i < data.length; i++) {
-           imgNameWithPattern = data[i].split(' ')[1];
-           imgName = imgNameWithPattern.split('.')[0]
-           imageSize = data[i].split(' ')[0];
-           imageX = imageSize.split('.')[0];
-           imageY = imageSize.split('.')[1];
-            li += '<div class="card" style="width:330px">' +
-                    '<div class="ImageInCard" style="height:'+ 330 * imageY / imageX + 'px">' +
-                      '<a data-fancybox="gallery" href="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
-                        '<img src="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true"/>' +
-                      '</a>' +
-                    '</div>' +
-                    // '<div class="TextInCard">' + imgName + '</div>' +
+        for (var dir_name in data) {
+          image_list = data[dir_name];
+          li += '<div class="ImageGrid">' +
+                      '<p>' + dir_name + '</p>'
+          for (var i = 0; i < image_list.length; i++) {
+             imgNameWithPattern = image_list[i].split(' ')[1];
+             imgName = imgNameWithPattern.split('.')[0]
+             imageSize = image_list[i].split(' ')[0];
+             imageX = imageSize.split('.')[0];
+             imageY = imageSize.split('.')[1];
+             li +=
+                  '<div class="card" style="width:330px">' +
+                      '<div class="ImageInCard" style="height:'+ 330 * imageY / imageX + 'px">' +
+                        '<a data-fancybox="gallery" href="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true" data-caption="' + imgName + '">' +
+                          '<img src="https://github.com/asdfv1929/BlogPhotos/blob/master/Images/' + imgNameWithPattern + '?raw=true"/>' +
+                        '</a>' +
+                      '</div>' +
+                      // '<div class="TextInCard">' + imgName + '</div>' +
                   '</div>'
-
+          }
+          li += '</div>'
         }
 
-        $(".ImageGrid").append(li);
-        $(".ImageGrid").lazyload();
+        $(".Dir").append(li);
+        $(".Dir").lazyload();
         this.minigrid();
+
     },
 
     minigrid: function() {
